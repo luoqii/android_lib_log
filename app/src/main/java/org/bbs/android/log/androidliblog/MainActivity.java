d1223e4d426df8a0e4e5399247dbc2eaa29a535f
bb.S <bysong@alibaba-inc.com>
2016-09-30 18:21:59 +0800

    add demo code.

--- app/src/main/java/org/bbs/android/log/androidliblog/MainActivity.java
+++ app/src/main/java/org/bbs/android/log/androidliblog/MainActivity.java
@@ -1,5 +1,6 @@
 package org.bbs.android.log.androidliblog;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
@@ -69,25 +70,31 @@ public class MainActivity extends AppCompatActivity {
 
     private void initLogger() {
         try {
-            Logger l = Logger.getAnonymousLogger();
-            Log.d(TAG, "isExternalStorageWritable:" + isExternalStorageWritable());
-            Log.d(TAG, "isExternalStorageReadable:" + isExternalStorageReadable());
+            if (BuildConfig.DEBUG) {
+                Logger l = Logger.getAnonymousLogger();
+                Log.d(TAG, "isExternalStorageWritable:" + isExternalStorageWritable());
+                Log.d(TAG, "isExternalStorageReadable:" + isExternalStorageReadable());
 
-            File sdcardLogDir = new File(Environment.getExternalStorageDirectory(),
-                    getApplication().getPackageName() + "/log");
-            sdcardLogDir = new File("/sdcard/log");
-            boolean mk = sdcardLogDir.mkdirs();
+                File sdcardLogDir = new File(Environment.getExternalStorageDirectory(),
+                        getApplication().getPackageName() + "/log");
+                sdcardLogDir = new File("/sdcard/log");
+                boolean mk = sdcardLogDir.mkdirs();
 //            Log.d(TAG, "mk:" + mk);
 
-            File logDir = new File(getExternalFilesDir(null), "log%g");
-            logDir = new File(sdcardLogDir, "log%g");
-            Log.d(TAG, "logDir:" + logDir.getParent());
-            FileHandler h = new FileHandler(logDir.getPath() + ".txt",
-                    1 * 1024 * 1024,
-                    5);            h.setFormatter(new Log.SimpleFormatter());
-            l.addHandler(h);
-            l.setLevel(Level.ALL);
-            Log.setLogger(l);
+                File logDir = new File(getExternalFilesDir(null), "log%g");
+                logDir = new File(sdcardLogDir, "log%g");
+                Log.d(TAG, "logDir:" + logDir.getParent());
+                FileHandler h = new FileHandler(logDir.getPath() + ".txt",
+                        1 * 1024 * 1024,
+                        5);
+                h.setFormatter(new Log.SimpleFormatter());
+                l.addHandler(h);
+                l.setLevel(Level.ALL);
+                Log.setLogger(l);
+            } else {
+                // for release show waring & errors
+                Log.setLevel(Log.WARN);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
